a3354cdeb3b584a99efa6316a1589d0d7c8049d4
Mark Zhong <mark.zhong@greenlake.co>
2026-09-08 08:43:37 -0400

    add seo tracking tool

--- src/lib/seoPages.ts
+++ src/lib/seoPages.ts
@@ -223,6 +223,13 @@ function layout(opts: {
     gtag("js", new Date());
     gtag("config", "${GA}");
   </script>
+  <script>
+    var ahrefs_analytics_script = document.createElement("script");
+    ahrefs_analytics_script.async = true;
+    ahrefs_analytics_script.src = "https://analytics.ahrefs.com/analytics.js";
+    ahrefs_analytics_script.setAttribute("data-key", "9/Dz6lvvNsgH5rTRNFNqMg");
+    document.getElementsByTagName("head")[0].appendChild(ahrefs_analytics_script);
+  </script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>${esc(opts.title)}</title>
   <meta name="description" content="${esc(opts.description)}" />
